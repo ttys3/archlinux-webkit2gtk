bdaccecae0dac3b2c0db80c62716382932f28f4d
ttyS3 <ttys3.rust@gmail.com>
2022-02-28 22:42:47 +0800

    chore: update gtk4.0 version to 2.34.6

---
+++ gtk4.0/cmp.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+bcompare orig PKGBUILD
+
+
+
---
+++ gtk4.0/get-orig.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+ curl -L -oorig https://github.com/archlinux/svntogit-packages/raw/packages/webkit2gtk/trunk/PKGBUILD
+
+
